a7686747b66f9469ee4441a345e03b606e9f110f
Rita <rita.kenji@gmail.com>
2025-09-02 14:46:01 +0200

    add comment for guidance and add delete-message paragraph and cancel-button on Color.jsx

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,5 @@
 import "./Color.css";
 
-//Set up a React component for displaying a single color card:
 export default function Color({ color, id, onDeleteColor }) {
   return (
     <div
@@ -13,9 +12,23 @@ export default function Color({ color, id, onDeleteColor }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <p className="delete-message">Really delete?</p>
+      {/* <button type="button" className="cancel-button" onClick={() =>}></button> */}
       <button type="button" onClick={() => onDeleteColor(id)}>
         Delete
       </button>
     </div>
   );
 }
+
+/* 
+What must happen: 
+I click the delete button ---> 1 <p> w message + cancel button pop up  AND  delete button moved to to right 
+
+How I get there:
+- 1st button onclick triggers onConfirmDelete
+- onConfirmDelete --> show 2 elements: 
+                              - message ---> <p className="delete-message">Really delete?</p>
+                              - cancel shows ---> <button type="button" className="cancel-button" onClick={() =>}></button>
+                              - delete button (use same one as initial))
+*/
