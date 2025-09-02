9534b70fea161430dff965180510b1089c2c5a75
Rita <rita.kenji@gmail.com>
2025-09-02 14:30:04 +0200

    add delete button and onClick on Color.jsx and pass id and onDeleteColor as props

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,7 +1,7 @@
 import "./Color.css";
 
 //Set up a React component for displaying a single color card:
-export default function Color({ color }) {
+export default function Color({ color, id, onDeleteColor }) {
   return (
     <div
       className="color-card"
@@ -13,6 +13,9 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <button type="button" onClick={() => onDeleteColor(id)}>
+        Delete
+      </button>
     </div>
   );
 }
