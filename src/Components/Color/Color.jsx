494a6cce10afdb5ace2edecc291076c3bff78172
Rita <rita.kenji@gmail.com>
2025-09-02 23:41:11 +0200

    re-add comments on acceptance criteria and tasks

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,26 @@
 import { useState } from "react";
 import ColorForm from "../ColorForm/ColorForm";
 import "./Color.css";
+
+/* 
+What happens:
+- click on edit button:
+                    > other buttons disappear
+                    > new form shows up role, hex, contrast color inputs + 'update color' button
+- click on update button:
+                    > info is saved and reflected in the theme
+*/
+
+/* 
+****** Acceptance Criteria
+- Each color card has an "Edit" button. ✅ 
+- Clicking the "Edit" button allows me to modify the role, hex value, and contrast text via a form.
+- The updated color information is reflected in the theme upon submission.
+
+****** Tasks
+- Introduce a state for the edit ✅ 
+- Reuse the ColorForm Component and display it within the Color Component when in edit mode ✅  */
+
 export default function Color({ color, id, onDeleteColor, onUpdateColor }) {
   const [mode, setMode] = useState("view");
 
