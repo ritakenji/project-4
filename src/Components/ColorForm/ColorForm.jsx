62e0d00f31ebc2ed5af0ae1a296e85b0a31f36a1
Rita <rita.kenji@gmail.com>
2025-09-02 14:02:31 +0200

    delete old comment w acceptance criteria and tasks, add new ones for issue 3

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,16 +1,15 @@
 /*
-***Acceptance Criteria***
-- Users can input a role, hex value, and contrast text via a form to add a new color to the theme.✅
-- The form should be prefilled with default values to guide user input.✅
-- Upon submission, the new color is added to the top of the colors and is displayed on a color card to confirm addition.✅
-- The inputs for hex and contrastText should include both color and text input types for easy and accurate color selection.✅
+*****Acceptance Criteria
+- Each color card displayed in the theme includes a "Delete" button for easy removal.
+- Clicking the "Delete" button should show a confirmation message before actually deleting
+- If there are no colors left in the theme after deletion, display a message encouraging users to add new colors.
 
-***Tasks***
-- Implement a ColorForm component that allows users to submit new colors to the theme.✅
-- Use a package to generate unique id's like nanoid or ui✅
-- Develop a ColorInput component to handle synchronized text and color inputs, ensuring that they reflect the same value. ( Controlled Inputs )✅
-^^^ need a component, render it 2x in Form✅
+****Tasks
+  Implement a function to handle the deletion of a color.
+- Introduce a state to handle the confirmation message
+- Reuse the .color-card-headline css rule for the confirm question, but maybe rename it to .color-card-hightlight
 */
+
 import "./ColorForm.css";
 import { ColorInput } from "./ColorInput";
 
