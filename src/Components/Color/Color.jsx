718cd8bcae7e1c470a7f3ee239e5cfdbf4ae3a0c
Rita <rita.kenji@gmail.com>
2025-09-02 15:24:43 +0200

    add cancel button, final delete button and confirmation message, aswell as isShown variable

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Color.css";
 
 /*
@@ -13,6 +14,12 @@ import "./Color.css";
 */
 
 export default function Color({ color, id, onDeleteColor }) {
+  const [isShown, setIsShown] = useState(false);
+
+  function handleDeleteConfirm() {
+    setIsShown(true);
+  }
+
   return (
     <div
       className="color-card"
@@ -24,11 +31,26 @@ export default function Color({ color, id, onDeleteColor }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
-      <p className="delete-message">Really delete?</p>
-      {/* <button type="button" className="cancel-button" onClick={() =>}></button> */}
-      <button type="button" onClick={() => onDeleteColor(id)}>
+
+      <button type="button" onClick={handleDeleteConfirm}>
         Delete
       </button>
+
+      {isShown && (
+        <>
+          <p className="delete-message">Really delete?</p>
+          <button
+            type="button"
+            className="cancel-button"
+            onClick={() => setIsShown(false)}
+          >
+            Cancel
+          </button>
+          <button type="button" onClick={() => onDeleteColor(id)}>
+            Delete
+          </button>
+        </>
+      )}
     </div>
   );
 }
@@ -39,8 +61,9 @@ I click the delete button ---> 1 <p> w message + cancel button pop up  AND  dele
 
 How I get there:
 - 1st button onclick triggers onConfirmDelete
-- onConfirmDelete --> show 2 elements: 
+- onConfirmDelete --> show 3 elements: 
                               - message ---> <p className="delete-message">Really delete?</p>
-                              - cancel shows ---> <button type="button" className="cancel-button" onClick={() =>}></button>
-                              - delete button (use same one as initial))
+                              - cancel button ---> <button type="button" className="cancel-button" onClick={() =>}>Cancel</button>
+                              - delete button ---> <button type="button" onClick={() => onDeleteColor(id)}>
+-
 */
